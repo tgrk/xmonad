0449c34d9d03056f520af562e9fac13311981222
Martin Wiso <martin.wiso@sumup.com>
2012-12-12 22:05:27 +0100

    Updating latest changes from Berlin

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,28 +5,38 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.IM as IM
+import XMonad.Layout.Minimize
+import XMonad.Layout.WindowNavigation
 import System.IO
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.ToggleLayouts
 import XMonad.Actions.Plane
+import XMonad.Layout.Grid
+import Data.Ratio ((%))
  
 myManageHook = composeAll 
     [ className =? "Vncviewer" --> doFloat
     , className =? "vmplayer" --> doFloat
     , className =? "Xmessage" --> doFloat
     , className =? "gnome-screenshot" --> doFloat
-    , className =? "Skype" --> doF(W.shift "1:chat")
-    , className =? "Pidgin" --> doF(W.shift "1:chat")
-    , className =? "Thunderbird" --> doF(W.shift "2:mail")
-    , className =? "Firefox" --> doF(W.shift "3:web")
-    , className =? "Chrome" --> doF(W.shift "3:web")
+--    , className =? "Skype" --> doF(W.shift "1:chat")
+    , className =? "google-chrome" --> doF(W.shift "2:web")
     ]
+
+-- Define the default layout
+skypeLayout = IM.withIM (1%7) (IM.And (ClassName "Skype")  (Role "MainWindow")) Grid
+normalLayout = windowNavigation $ minimize $ avoidStruts $ onWorkspace "1:chat" skypeLayout $ layoutHook defaultConfig
+myLayout = toggleLayouts (Full) normalLayout
  
 main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar /home/tgrk/.xmobarrc"
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/wiso/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
                         <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , workspaces = ["1:chat", "2:mail", "3:web"] ++ map show [4..10]
+--        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = myLayout
+        , workspaces = ["1:chat", "2:web"] ++ map show [3..9]
         , logHook = dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
@@ -37,14 +47,15 @@ main = do
 	, normalBorderColor  = "#000000"
         , focusedBorderColor = "#666666"
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
           
         , ((mod4Mask, xK_e), spawn "emacs")
-        , ((mod4Mask, xK_f), spawn "firefox")
-        , ((mod4Mask, xK_m), spawn "thunderbird")
-        , ((mod4Mask, xK_g), spawn "gimp")
+        , ((mod4Mask, xK_f), spawn "google-chrome")
         , ((mod4Mask, xK_v), spawn "gnome-alsamixer")
         , ((mod4Mask, xK_x), spawn "gnome-terminal")
+	, ((mod4Mask .|. shiftMask, xK_h), spawn "sudo pm-hibernate")
+	, ((mod4Mask .|. shiftMask, xK_r), spawn "sudo reboot")
+	, ((mod4Mask .|. shiftMask, xK_s), spawn "sudo shutdown -h now")
 
 	, ((mod4Mask, xK_Right), planeMove (Lines 1) Circular ToRight)
 	, ((mod4Mask, xK_Left),  planeMove (Lines 1) Circular ToLeft)
