190dfb0afd30a17e8c4fbb26006da664042370ae
Martin Wiso <martin@wiso.cz>
2016-04-12 21:11:08 +0200

    Add helper script for displaying battery status

---
+++ scripts/xmobar_battery.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PWR=$(acpi -b | cut -f4 -d' ' | tr -d '%' | tr -d ',')
+
+if [ $PWR -gt 0 ] ; then
+  COLOR='red'
+  SYM='%'
+fi
+
+if [ $PWR -gt 20 ] ; then
+  COLOR='yellow'
+fi
+
+if [ $PWR -gt 60 ] ; then
+  COLOR='green'
+fi
+
+if [[ $(acpi -a) == 'Adapter 0: on-line' ]] ; then
+  COLOR='cadetblue'
+  PWR='AC'
+  SYM=''
+fi
+
+echo "<fc=$COLOR>$PWR$SYM</fc>"
